539efa1abf0795b2c962591c09e50c33b28ac08e
Pablo de Andres <pablodeandresolano@gmail.com>
2022-08-18 15:02:49 +0200

    Add: Comment model

---
+++ models/Comment.model.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    game: {
+      type: Schema.Types.ObjectId,
+      ref: "Game",
+    },
+    isEdited: {
+      type: Boolean,
+      default: false,
+    },
+    likes: {
+      type: Number,
+      default: 0,
+    },
+    dislikes: {
+      type: Number,
+      default: 0,
+    },
+    isLiked: {
+      type: Boolean,
+      default: false,
+    },
+    isDisliked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Comment = model("Comment", userSchema);
+
+module.exports = Comment;
