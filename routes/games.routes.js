76125f594d19bfe23326a512e95341aa53b16982
Pablo de Andres <pablodeandresolano@gmail.com>
2022-08-24 21:57:17 +0200

    Valorations routes

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -4,12 +4,17 @@ const Comment = require("../models/Comment.model");
 
 const isAuthenticated = require("../middlewares/isAuthenticated");
 
-//* POST "/api/games/:gameId" => Add one game to the DB
+//* POST "/api/games/:gameId" => Add one game valoration  to the DB
 router.post("/:gameId", isAuthenticated, async (req, res, next) => {
   const { gameId } = req.params;
+  const { valoration } = req.body;
+  const { _id } = req.payload;
+
   try {
     const newGame = await Game.create({
-      id: gameId,
+      valoration: valoration,
+      apiId: gameId,
+      creator: _id,
     });
     res.json(newGame);
   } catch (error) {
@@ -18,13 +23,15 @@ router.post("/:gameId", isAuthenticated, async (req, res, next) => {
   }
 });
 
-//* GET "/api/games/:gameId" => Find one game in the DB
+//* GET "/api/games/:gameId" => Find user game valoration  in the DB
 router.get("/:gameId", isAuthenticated, async (req, res, next) => {
   const { gameId } = req.params;
-
+  const { _id } = req.payload;
   try {
-    const findGame = await Game.findOne({ id: gameId }); // se ha cambiado el findById por findOne hasta rellenar la base de datos
-    console.log(findGame);
+    const findGame = await Game.findOne({ creator: _id, apiId: gameId }).select(
+      "valoration"
+    );
+
     res.json(findGame);
   } catch (error) {
     console.log(error);
@@ -32,17 +39,37 @@ router.get("/:gameId", isAuthenticated, async (req, res, next) => {
   }
 });
 
-//* DELETE "/api/games/:gameId" => Delete one game from the DB
-router.delete("/:gameId", isAuthenticated, async (req, res, next) => {
+//* GET "/api/games/:gameId/valorations" =>  Find all valorations of a game
+router.get("/:gameId/valorations", isAuthenticated, async (req, res, next) => {
   const { gameId } = req.params;
-  console.log(gameId);
   try {
-    await Game.findByIdAndDelete(gameId);
-    res.status(200).json({ message: "Game deleted from the DB" });
+    const findGame = await Game.find({ apiId: gameId }).select("valoration");
+    res.json(findGame);
   } catch (error) {
     console.log(error);
     next(error);
   }
 });
+
+//*PATCH "api/games/:gameId" => Edit one valoration of a game
+router.patch("/:gameId", isAuthenticated, async (req, res, next) => {
+  const { gameId } = req.params;
+  const { _id } = req.payload;
+  const { valoration } = req.body;
+  try {
+    const GameValoration = await Game.findOneAndUpdate(
+      { creator: _id, apiId: gameId },
+      {
+        valoration: valoration,
+      }
+    );
+    console.log("game valoration edit ", GameValoration);
+
+    res.status(200).json();
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 module.exports = router;
