19b81c73551f3162bbc041d3093897cdae20978a
Pablo de Andres <pablodeandresolano@gmail.com>
2022-08-24 21:56:53 +0200

    Clean code

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,21 +2,19 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const Comment = require("../models/Comment.model");
 const isAuthenticated = require("../middlewares/isAuthenticated");
-
-//* GET "/api/email" =>  Find user in DB
-router.get("/my-profile", isAuthenticated, async (req, res, next) => {
+//* GET "/api/profile/favourites" =>  find user's favourite games
+router.get("/favourites", isAuthenticated, async (req, res, next) => {
   console.log("req.payload", req.payload);
   const { _id } = req.payload;
   try {
-    const findUser = await User.findById(_id);
-    console.log(findUser);
-    res.json(findUser);
+    const user = await User.findById(_id).select("favourites");
+    console.log("finduser", user);
+    res.json(user);
   } catch (error) {
     console.log("Error user /", error);
     next(error);
   }
 });
-
 //* GET "/api/profile/:id" =>  Find user in DB
 router.get("/:id", isAuthenticated, async (req, res, next) => {
   const { id } = req.params;
@@ -30,7 +28,7 @@ router.get("/:id", isAuthenticated, async (req, res, next) => {
   }
 });
 
-//* PATCH "/api/profile/edit" => Edit user in DB
+//* PATCH "/api/profile/:id/edit" => Edit user in DB
 router.patch("/:id/edit", isAuthenticated, async (req, res, next) => {
   const { id } = req.params;
   const { name, email, avatar } = req.body;
@@ -60,57 +58,7 @@ router.patch("/close-account", isAuthenticated, async (req, res, next) => {
   }
 });
 
-//TODO Como sumar likes y dislikes en findByIdAndUpdate
-//* PATCH /api/profile/like/:commentId" => Add comment to likedComments
-router.patch("/like/:commentId", isAuthenticated, async (req, res, next) => {
-  const { commentId } = req.params;
-  const { _id } = req.payload;
-
-  try {
-    //comprobar si el comentario ya tiene el like del usuario
-    const isCommentLiked = await Comment.findOne({ likes: _id });
-    console.log(isCommentLiked);
-    //si lo tiene quitarlo
-    // si no lo tiene comprobar que si tiene dislike
-    //si lo tiene quitarlo y agregar like
-
-    res.status(200).json();
-  } catch (error) {
-    next(error);
-  }
-});
-
-//* PATCH /api/profile/dislike/:commentId" => Add comment to dislikedComments
-router.patch("/dislike/:commentId", isAuthenticated, async (req, res, next) => {
-  const { commentId } = req.params;
-  const { _id } = req.payload;
-
-  try {
-    const isCommentLiked = await User.findById(_id, {
-      likedComments: commentId,
-    });
-    if (isCommentLiked) {
-      await Comment.findByIdAndUpdate(commentId, {
-        $inc: { dislikes: 1 },
-        $inc: { likes: -1 },
-      });
-    } else {
-      await Comment.findByIdAndUpdate(commentId, {
-        $inc: { dislikes: 1 },
-      });
-    }
-    await User.findByIdAndUpdate(_id, {
-      $addToSet: { dislikedComments: commentId },
-      $pull: { likedComments: commentId },
-    });
-
-    res.status(200).json();
-  } catch (error) {
-    next(error);
-  }
-});
-
-//* PATCH "/api/profile/like/:gameId" => Add a  game to user favourite games
+//* PATCH "/api/profile/favourites/:gameId" => Add a  game to user favourite games
 router.patch("/favourites/:gameId", isAuthenticated, async (req, res, next) => {
   const { gameId } = req.params;
   const { _id } = req.payload;
@@ -134,7 +82,5 @@ router.patch("/favourites/:gameId", isAuthenticated, async (req, res, next) => {
     next(error);
   }
 });
-
-
 
 module.exports = router;
