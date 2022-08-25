27bec155c705aa3e34e326b0019e424e4a0281a7
Pablo de Andres <pablodeandresolano@gmail.com>
2022-08-26 01:48:22 +0200

    Move route for fix bug

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,13 +2,44 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const Comment = require("../models/Comment.model");
 const isAuthenticated = require("../middlewares/isAuthenticated");
+
+
+//* PATCH "/api/profile/favourites/:gameId" => Add a  game to user favourite games
+router.patch("/favourites/:gameId", isAuthenticated, async (req, res, next) => {
+  const { gameId } = req.params;
+  const { _id } = req.payload;
+  const { gameImg, gameName } = req.body;
+
+  try {
+    const user = await User.findById(_id);
+    const isFavourite =
+      user.favourites.filter((eachFavourite) => {
+        console.log("eachFavourite", eachFavourite);
+        return eachFavourite.gameId === gameId;
+      }).length > 0;
+    console.log("FAVOURITE", isFavourite);
+    if (isFavourite) {
+      const removeFromFavourite = await User.findByIdAndUpdate(_id, {
+        $pull: { favourites: { gameId, gameImg, gameName } },
+      });
+      console.log("elemento quitado de favoritos", removeFromFavourite);
+    } else {
+      const addToFavourite = await User.findByIdAndUpdate(_id, {
+        $addToSet: { favourites: { gameId, gameImg, gameName } },
+      });
+      console.log("elemento agregado a favoritos", addToFavourite);
+    }
+    res.status(200).json();
+  } catch (error) {
+    next(error);
+  }
+});
 //* GET "/api/profile/favourites" =>  find user's favourite games
 router.get("/favourites", isAuthenticated, async (req, res, next) => {
-  console.log("req.payload", req.payload);
   const { _id } = req.payload;
   try {
     const user = await User.findById(_id).select("favourites");
-    console.log("finduser", user);
+    console.log("USER", user);
     res.json(user);
   } catch (error) {
     console.log("Error user /", error);
@@ -20,7 +51,6 @@ router.get("/:id", isAuthenticated, async (req, res, next) => {
   const { id } = req.params;
   try {
     const findUser = await User.findById(id);
-    console.log(findUser);
     res.json(findUser);
   } catch (error) {
     console.log("Error user /", error);
@@ -52,31 +82,6 @@ router.patch("/close-account", isAuthenticated, async (req, res, next) => {
       accountClosed: true,
     });
     res.status(200).json({ message: "Acount closed ! " });
-  } catch (error) {
-    next(error);
-  }
-});
-
-//* PATCH "/api/profile/favourites/:gameId" => Add a  game to user favourite games
-router.patch("/favourites/:gameId", isAuthenticated, async (req, res, next) => {
-  const { gameId } = req.params;
-  const { _id } = req.payload;
-  try {
-    const user = await User.findById(_id);
-    console.log(user.favourites);
-
-    if (user.favourites.includes(gameId)) {
-      const removeFromFavourite = await User.findByIdAndUpdate(_id, {
-        $pull: { favourites: gameId },
-      });
-      console.log("elemento quitado de favoritos", removeFromFavourite);
-    } else {
-      const addToFavourite = await User.findByIdAndUpdate(_id, {
-        $addToSet: { favourites: gameId },
-      });
-      console.log("elemento agregado a favoritos", addToFavourite);
-    }
-    res.status(200).json();
   } catch (error) {
     next(error);
   }
